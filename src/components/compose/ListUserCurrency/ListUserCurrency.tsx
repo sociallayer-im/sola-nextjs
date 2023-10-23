af9119142c2e723e5cffe7af031ca32b9f8c3422
qieman <253058016@qq.com>
2023-10-23 15:42:44 +0800

    update erc20 assets

--- src/components/compose/ListUserCurrency/ListUserCurrency.tsx
+++ src/components/compose/ListUserCurrency/ListUserCurrency.tsx
@@ -1,4 +1,4 @@
-import {useState, useContext, useEffect} from 'react'
+import {useContext, useEffect, useState} from 'react'
 import styles from './ListUserCurrency.module.sass'
 import Alchemy, {CurrencyBalance, ExplorerUrls} from "@/service/alchemy/alchemy";
 import userContext from "@/components/provider/UserProvider/UserContext";
@@ -6,9 +6,10 @@ import LangContext from "@/components/provider/LangProvider/LangContext";
 import Image from "next/image";
 import {Profile} from "@/service/solas";
 
-function ComponentName({profile}: {profile: Profile}) {
+function ComponentName({profile}: { profile: Profile }) {
     const {user} = useContext(userContext)
     const {lang} = useContext(LangContext)
+    const [ready, setReady] = useState<boolean>(false)
 
     const [balance, setBalance] = useState<CurrencyBalance>({
         eth: '0.0000',
@@ -18,38 +19,86 @@ function ComponentName({profile}: {profile: Profile}) {
         astar: '0.0000'
     })
 
-    useEffect(() => {
+    const [erc20Balance, setErc20Balance] = useState<any | null>(null)
+
+    const getData = async () => {
         if (profile.address) {
-            Alchemy.getBalance(profile.address).then((res) => {
-                console.log(res)
-                setBalance(res)
-            })
+            await Promise.all([
+                Alchemy.getBalance(profile.address).then((res) => {
+                    setBalance(res)
+                }),
+                Alchemy.getErc20Assets(profile.address).then((res) => {
+                    setErc20Balance(res)
+                })
+            ])
+            setReady(true)
         }
-    }, [user])
+    }
+
+    useEffect(() => {
+        getData()
+    }, [])
 
     return (<div>
         <div className={`${styles.row} ${styles.title}`}>
             <div>{lang['Profile_Tab_Asset']}</div>
             <div>{lang['Profile_Asset_Amount']}</div>
         </div>
-        {
+        {ready && erc20Balance &&
             Object.keys(balance).map((key) => {
-
-              return (balance as any)[key] === '0.0000' && key !== 'eth'
-                  ? null
-                  : <a className={`${styles.row} ${styles.item}`}
-                       key={key}
-                       target={'_blank'}
-                       href={(ExplorerUrls as any)[key] + profile.address!}>
-                      <div className={styles.label}>
-                          <Image className={styles.icon}
-                                 alt={key}
-                                 src={`/images/tokens/${key}.png`}
-                                 width={22} height={22} />
-                          {key.toUpperCase()}
-                      </div>
-                      <div>{(balance as any)[key]}</div>
-                  </a>
+                return <div key={key}>
+                    {(balance as any)[key] === '0.0000' && key !== 'eth' ?
+                       null:
+                        <a className={`${styles.row} ${styles.item}`}
+                           target={'_blank'}
+                           href={(ExplorerUrls as any)[key] + profile.address!}>
+                            <div className={styles.label}>
+                                <Image className={styles.icon}
+                                       alt={key}
+                                       src={`/images/tokens/${key}.png`}
+                                       width={22} height={22}/>
+                                {key.toUpperCase()}
+                            </div>
+                            <div>{(balance as any)[key]}</div>
+                        </a>
+                    }
+                    {(erc20Balance as any)[key].usdt !== '0.0000' &&
+                        <a href="#" className={`${styles.row} ${styles.item}`}>
+                            <div className={styles.label}>
+                                <div className={styles.erc20}>
+                                    <Image className={styles.icon}
+                                           alt={key}
+                                           src={`/images/usdt.png`}
+                                           width={22} height={22}/>
+                                    <Image className={styles['chain-icon']}
+                                           alt={key}
+                                           src={`/images/tokens/${key}.png`}
+                                           width={22} height={22}/>
+                                </div>
+                                {'USDT'}
+                            </div>
+                            <div>{(erc20Balance as any)[key].usdt}</div>
+                        </a>
+                    }
+                    {(erc20Balance as any)[key].usdc !== '0.0000' &&
+                        <a href="#" className={`${styles.row} ${styles.item}`}>
+                            <div className={styles.label}>
+                                <div className={styles.erc20}>
+                                    <Image className={styles.icon}
+                                           alt={key}
+                                           src={`/images/usdc.png`}
+                                           width={22} height={22}/>
+                                    <Image className={styles['chain-icon']}
+                                           alt={key}
+                                           src={`/images/tokens/${key}.png`}
+                                           width={22} height={22}/>
+                                </div>
+                                {'USDC'}
+                            </div>
+                            <div>{(erc20Balance as any)[key].usdc}</div>
+                        </a>
+                    }
+                </div>
             })
         }
     </div>)
--- src/service/alchemy/alchemy.ts
+++ src/service/alchemy/alchemy.ts
@@ -1,7 +1,7 @@
 // 'ReadMe-API-Explorer'
 
-import { AlchemyMultichainClient } from './alchemy-multichain-client';
-import { Network,  } from 'alchemy-sdk';
+import {AlchemyMultichainClient} from './alchemy-multichain-client';
+import {Network,} from 'alchemy-sdk';
 
 export interface CurrencyBalance {
     eth: string,
@@ -27,7 +27,32 @@ export const ExplorerUrls = {
     arb: 'https://arbiscan.io/address/',
     opt: 'https://optimistic.etherscan.io/address/',
     astar: 'https://astar.subscan.io/account/'
-} as {[key : string]: string }
+} as { [key: string]: string }
+
+export interface Erc20Asset {
+    usdt: string,
+    usec: string,
+
+}
+
+export const erc20List = {
+    eth: {
+        usdt: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+        usdc: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+    },
+    matic: {
+        usdt: '0xc2132D05D31c914a87C6611C10748AEb04B58e8F',
+        usdc: '0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174'
+    },
+    arb: {
+        usdt: '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9',
+        usdc: '0xaf88d065e77c8cC2239327C5EDb3A432268e5831'
+    },
+    opt: {
+        usdt: '0x94b008aA00579c1307B0EF2c499aD98a8ce58e58',
+        usdc: '0x0b2C639c533813f4Aa9D7837CAf62653d097Ff85'
+    }
+}
 
 class Alchemy {
     alchemy: AlchemyMultichainClient
@@ -40,16 +65,16 @@ class Alchemy {
 
         const overrides = {
             // TODO: Replace with your API keys.
-            [Network.MATIC_MAINNET]: { apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY, maxRetries: 10 }, // Replace with your Matic Alchemy API key.
-            [Network.ARB_MAINNET]: { apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY }, // Replace with your Arbitrum Alchemy API key.
-            [Network.OPT_MAINNET]: { apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY }, // Replace with your Arbitrum Alchemy API key.
-            [Network.ASTAR_MAINNET]: { apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY } // Replace with your Arbitrum Alchemy API key.
+            [Network.MATIC_MAINNET]: {apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY, maxRetries: 10}, // Replace with your Matic Alchemy API key.
+            [Network.ARB_MAINNET]: {apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY}, // Replace with your Arbitrum Alchemy API key.
+            [Network.OPT_MAINNET]: {apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY}, // Replace with your Arbitrum Alchemy API key.
+            [Network.ASTAR_MAINNET]: {apiKey: process.env.NEXT_PUBLIC_ALCHEMY_KEY} // Replace with your Arbitrum Alchemy API key.
         };
         this.alchemy = new AlchemyMultichainClient(defaultConfig, overrides);
     }
 
     async getBalance(owner: string): Promise<CurrencyBalance> {
-        let balance:CurrencyBalance = {
+        let balance: CurrencyBalance = {
             eth: '',
             matic: '',
             arb: '',
@@ -58,19 +83,19 @@ class Alchemy {
         }
 
         const fetch = await Promise.all([
-            this.alchemy.forNetwork(Network.ETH_MAINNET).core.getBalance(owner).then(res=> {
+            this.alchemy.forNetwork(Network.ETH_MAINNET).core.getBalance(owner).then(res => {
                 balance.eth = (Number(res.toString()) / 1000000000000000000).toFixed(4)
             }),
-            this.alchemy.forNetwork(Network.POLYGONZKEVM_MAINNET).core.getBalance(owner).then(res=> {
+            this.alchemy.forNetwork(Network.MATIC_MAINNET).core.getBalance(owner).then(res => {
                 balance.matic = (Number(res.toString()) / 1000000000000000000).toFixed(4)
             }),
-            this.alchemy.forNetwork(Network.ARB_MAINNET).core.getBalance(owner).then(res=> {
+            this.alchemy.forNetwork(Network.ARB_MAINNET).core.getBalance(owner).then(res => {
                 balance.arb = (Number(res.toString()) / 1000000000000000000).toFixed(4)
             }),
-            this.alchemy.forNetwork(Network.OPT_MAINNET).core.getBalance(owner).then(res=> {
+            this.alchemy.forNetwork(Network.OPT_MAINNET).core.getBalance(owner).then(res => {
                 balance.opt = (Number(res.toString()) / 1000000000000000000).toFixed(4)
             }),
-            this.alchemy.forNetwork(Network.ASTAR_MAINNET).core.getBalance(owner).then(res=> {
+            this.alchemy.forNetwork(Network.ASTAR_MAINNET).core.getBalance(owner).then(res => {
                 balance.astar = (Number(res.toString()) / 1000000000000000000).toFixed(4)
             }),
         ])
@@ -79,8 +104,8 @@ class Alchemy {
     }
 
     async getEnsBalance(owner: string): Promise<NftDetail[]> {
-        const list = await this.alchemy.forNetwork(Network.ETH_MAINNET).nft.getNftsForOwner(owner, {contractAddresses:['0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85']})
-        return  list.ownedNfts.map((item: any) => {
+        const list = await this.alchemy.forNetwork(Network.ETH_MAINNET).nft.getNftsForOwner(owner, {contractAddresses: ['0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85']})
+        return list.ownedNfts.map((item: any) => {
             return {
                 title: item.title,
                 image: item.rawMetadata?.image_url,
@@ -94,8 +119,8 @@ class Alchemy {
     }
 
     async getMaodaoNft(owner: string): Promise<NftDetail[]> {
-        const list = await this.alchemy.forNetwork(Network.ETH_MAINNET).nft.getNftsForOwner(owner, {contractAddresses:['0xcdb7C1a6fE7e112210CA548C214F656763E13533']})
-        return  list.ownedNfts.map((item: any) => {
+        const list = await this.alchemy.forNetwork(Network.ETH_MAINNET).nft.getNftsForOwner(owner, {contractAddresses: ['0xcdb7C1a6fE7e112210CA548C214F656763E13533']})
+        return list.ownedNfts.map((item: any) => {
             return {
                 title: item.title,
                 image: item.rawMetadata?.image,
@@ -110,8 +135,8 @@ class Alchemy {
 
     async getSeedaoNft(owner: string): Promise<NftDetail[]> {
         // toto replace owner address
-        const list = await this.alchemy.forNetwork(Network.ETH_MAINNET).nft.getNftsForOwner('0x332345477db00239f88ca2eb015b159750cf3c44', {contractAddresses:['0x30093266e34a816a53e302be3e59a93b52792fd4']})
-        return  list.ownedNfts.map((item: any) => {
+        const list = await this.alchemy.forNetwork(Network.ETH_MAINNET).nft.getNftsForOwner('0x332345477db00239f88ca2eb015b159750cf3c44', {contractAddresses: ['0x30093266e34a816a53e302be3e59a93b52792fd4']})
+        return list.ownedNfts.map((item: any) => {
             return {
                 title: `SeeDAO Seed NFT #${item.tokenId}`,
                 image: item.rawMetadata?.image,
@@ -130,7 +155,44 @@ class Alchemy {
             .nft
             .getOwnersForNft('0xcdb7c1a6fe7e112210ca548c214f656763e13533', nftId)
 
-        return  response.owners[0] || null
+        return response.owners[0] || null
+    }
+
+    async getErc20Assets (ownerAddress: string) {
+        const balance : any = {
+            eth:{usdt: '0.0000', usdc: '0.0000'},
+            matic:{usdt: '0.0000', usdc: '0.0000'},
+            arb:{usdt: '0.0000', usdc: '0.0000'},
+            opt:{usdt: '0.0000', usdc: '0.0000'},
+            astar:{usdt: '0.0000', usdc: '0.0000'}
+        }
+
+        console.log('ownerAddressownerAddressownerAddress', ownerAddress)
+
+        const assets = await Promise.all([
+            this.alchemy.forNetwork(Network.ETH_MAINNET).core.getTokenBalances(ownerAddress, [erc20List.eth.usdt, erc20List.eth.usdc]).then((res: any) => {
+                console.log('eth', res)
+                balance.eth.usdt = (Number(res.tokenBalances[0].tokenBalance) / 1000000).toFixed(4)
+                balance.eth.usdc = (Number(res.tokenBalances[1].tokenBalance) / 1000000).toFixed(4)
+            }),
+            this.alchemy.forNetwork(Network.MATIC_MAINNET).core.getTokenBalances(ownerAddress, [erc20List.matic.usdt, erc20List.matic.usdc]).then((res: any) => {
+                console.log('matic', res)
+                balance.matic.usdt = (Number(res.tokenBalances[0].tokenBalance) / 1000000).toFixed(4)
+                balance.matic.usdc = (Number(res.tokenBalances[1].tokenBalance) / 1000000).toFixed(4)
+            }),
+            this.alchemy.forNetwork(Network.ARB_MAINNET).core.getTokenBalances(ownerAddress, [erc20List.arb.usdt, erc20List.arb.usdc]).then((res: any) => {
+                console.log('arb', res)
+                balance.arb.usdt = (Number(res.tokenBalances[0].tokenBalance) / 1000000).toFixed(4)
+                balance.arb.usdc = (Number(res.tokenBalances[1].tokenBalance) / 1000000).toFixed(4)
+            }),
+            this.alchemy.forNetwork(Network.OPT_MAINNET).core.getTokenBalances(ownerAddress, [erc20List.opt.usdt, erc20List.opt.usdc]).then((res: any) => {
+                console.log('opt', res)
+                balance.opt.usdt = (Number(res.tokenBalances[0].tokenBalance) / 1000000).toFixed(4)
+                balance.opt.usdc = (Number(res.tokenBalances[1].tokenBalance) / 1000000).toFixed(4)
+            }),
+        ])
+
+        return balance
     }
 }
 
