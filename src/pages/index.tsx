5c5aececec7429fbf4de9bb86299e7da6e25f3c4
qieman <253058016@qq.com>
2023-11-07 18:23:11 +0800

    add home page redirect

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,21 @@
 import Page from "@/pages/event/index"
+import {useRouter as useClientRouter } from "next/navigation";
+import MapPage from '@/pages/event/[groupname]/map'
 
-export default Page
+export default function HomePage (props: {domain: string}) {
+    const routerClient = useClientRouter()
+
+    const needRedirect = (props.domain.includes('3000'))
+
+    if (needRedirect && typeof window !== 'undefined') {
+        routerClient.push('/event/istanbul2023/map')
+    }
+
+    return needRedirect ? <MapPage /> : <Page />
+}
+
+export const getServerSideProps: any = (async (context: any) => {
+    const domain = context.req.rawHeaders[1]
+    return { props: { domain } }
+})
+
