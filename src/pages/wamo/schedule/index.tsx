95c3b1db4349420de45ef63eca9fa48f9dac5669
qieman <253058016@qq.com>
2023-12-13 14:18:20 +0800

    update wamo schedule

--- src/pages/wamo/schedule/index.tsx
+++ src/pages/wamo/schedule/index.tsx
@@ -65,6 +65,13 @@ function ComponentName(props: { group: Group }) {
     const [currYear, setCurrYear] = useState(new Date().getFullYear())
     const [currTag, setCurrTag] = useState<string[]>([])
 
+    // touch on pc
+    const touchStart = useRef(false)
+    const touchStartX = useRef(0)
+    const touchStartY = useRef(0)
+    const touchStartScrollLeft = useRef(0)
+    const touchStartScrollTop = useRef(0)
+
     const slideToToday = (init=false) => {
         const scrollBar1 = scroll1Ref.current
         const scrollBar2 = scroll2Ref.current
@@ -146,6 +153,42 @@ function ComponentName(props: { group: Group }) {
             // }
         }
 
+        const checkMousedown = (e: any) => {
+            e.preventDefault()
+            console.log('down')
+            touchStart.current = true
+            touchStartX.current = e.clientX
+            touchStartY.current = e.clientY
+            touchStartScrollLeft.current = scroll2Ref.current.scrollLeft
+            touchStartScrollTop.current = scroll2Ref.current.scrollTop
+        }
+
+        const checkMouseup = (e: any) => {
+            touchStart.current = false
+        }
+
+        const checkMousemove = (e: any) => {
+            if (touchStart.current) {
+                const offsetX =  e.clientX - touchStartX.current
+                const offsetY = e.clientY - touchStartY.current
+                    console.log('mousemove', offsetX, offsetY)
+                scroll2Ref.current.scrollLeft = touchStartScrollLeft.current - offsetX
+                scroll2Ref.current.scrollTop = touchStartScrollTop.current - offsetY
+            }
+        }
+
+        const checkTouch = () => {
+            scroll2Ref.current.addEventListener('mousedown', checkMousedown)
+
+            scroll2Ref.current.addEventListener('mouseup', checkMouseup)
+
+            scroll2Ref.current.addEventListener('mousemove', checkMousemove)
+
+            scroll2Ref.current.addEventListener('mouseleave', checkMouseup)
+        }
+
+        checkTouch()
+
         if(scroll1Ref.current && scroll2Ref.current) {
             const scrollBar1 = scroll1Ref.current
             const scrollBar2 = scroll2Ref.current
@@ -158,6 +201,10 @@ function ComponentName(props: { group: Group }) {
             return () => {
                 scrollBar1?.removeEventListener('scroll', checkScroll)
                 scrollBar2?.removeEventListener('scroll', checkScroll2)
+                scrollBar2?.removeEventListener('mousedown', checkMousedown)
+                scrollBar2?.removeEventListener('mouseup', checkMouseup)
+                scrollBar2?.removeEventListener('mousemove', checkMousemove)
+                scrollBar2?.removeEventListener('mouseleave', checkMouseup)
             }
         }
     }, [scroll1Ref, scroll2Ref])
