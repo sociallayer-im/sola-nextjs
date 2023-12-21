50d089f8998d1b83f2d326ad4e6fe74d3af97b80
qieman <253058016@qq.com>
2023-12-21 15:15:55 +0800

    update

--- src/pages/event/success/[eventid]/index.tsx
+++ src/pages/event/success/[eventid]/index.tsx
@@ -1,9 +1,9 @@
-import {useRouter, useParams} from 'next/navigation'
+import {useParams, useRouter} from 'next/navigation'
 import Link from 'next/link'
 import {useContext, useEffect, useRef, useState} from 'react'
 import {Event, queryEventDetail} from "@/service/solas";
 import LangContext from "@/components/provider/LangProvider/LangContext";
-import {formatTime ,formatTimeWithTimezone} from "@/hooks/formatTime";
+import {formatTime, formatTimeWithTimezone} from "@/hooks/formatTime";
 import QRcode from "@/components/base/QRcode";
 import AppButton from "@/components/base/AppButton/AppButton";
 import saveCard from "@/utils/html2png";
@@ -31,7 +31,7 @@ function CreateEventSuccess() {
             setEvent(res)
         }
 
-       if (params?.eventid) {
+        if (params?.eventid) {
             fetchData()
         }
     }, [params])
@@ -64,67 +64,75 @@ function CreateEventSuccess() {
 
     return (<>
         <div className={'create-event-success-page'}>
-            <div className={'center'}><Link className={'done'} href={`/event/detail/${params?.eventid}`}>Done</Link></div>
+            <div className={'center'}><Link className={'done'} href={`/event/detail/${params?.eventid}`}>Done</Link>
+            </div>
             <div className={'title'}>{lang['IssueFinish_Title']}</div>
             {event &&
                 <>
-                    <div className={'event-share-card'} ref={card}>
-                        <img src={event.cover_url} className={'cover'} alt=""/>
-                        <div className={'name'}>{event.title}</div>
-                        {!!event.start_time &&
-                            <div className={'time'}>
-                                <i className={'icon-calendar'}/>
-                                <div className={'start-time'}>
-                                    {event.timezone ?
-                                        formatTimeWithTimezone(event.start_time, event.timezone)
-                                        : formatTime(event.start_time)
+                    <div className={'event-share-card-wrapper'}>
+                        <div className={'event-share-card'} ref={card}>
+                            <img src={event.cover_url} className={'cover'} alt=""/>
+                            <div className={'name'}>{event.title}</div>
+                            {!!event.start_time &&
+                                <div className={'time'}>
+                                    <i className={'icon-calendar'}/>
+                                    <div className={'start-time'}>
+                                        {event.timezone ?
+                                            formatTimeWithTimezone(event.start_time, event.timezone)
+                                            : formatTime(event.start_time)
+                                        }
+                                    </div>
+                                    {
+                                        event.end_time &&
+                                        <>
+                                            <span>—</span>
+                                            <div className={'end-time'}> {
+                                                event.timezone ?
+                                                    formatTimeWithTimezone(event.end_time, event.timezone)
+                                                    : formatTime(event.end_time)
+                                            }</div>
+                                        </>
                                     }
                                 </div>
-                                {
-                                    event.end_time &&
-                                    <>
-                                        <span>—</span>
-                                        <div className={'end-time'}> {
-                                            event.timezone ?
-                                                formatTimeWithTimezone(event.end_time, event.timezone)
-                                                : formatTime(event.end_time)
-                                        }</div>
-                                    </>
-                                }
-                            </div>
-                        }
-                        {!!event.timezone &&
-                            <div className={'time'}>
-                                <div className={'event-timezone'}>{mapTimezone(event.timezone).label}</div>
-                            </div>
-                        }
-                        {
-                            !!event.location && <div className={'time'}>
-                                <i className={'icon-Outline'}/>
-                                <div>{event.location}{event.formatted_address ? `(${event.formatted_address})` : ''}</div>
-                            </div>
-                        }
-                        {
-                            !!event.meeting_url && <div className={'time'}>
-                                <i className={'icon-link'}/>
-                                <div>{getMeetingName(event.meeting_url)}</div>
-                            </div>
-                        }
+                            }
+                            {!!event.timezone &&
+                                <div className={'time'}>
+                                    <div className={'event-timezone'}>{mapTimezone(event.timezone).label}</div>
+                                </div>
+                            }
+                            {
+                                !!event.location && <div className={'time'}>
+                                    <i className={'icon-Outline'}/>
+                                    <div>{event.location}{event.formatted_address ? `(${event.formatted_address})` : ''}</div>
+                                </div>
+                            }
+                            {
+                                !!event.meeting_url && <div className={'time'}>
+                                    <i className={'icon-link'}/>
+                                    <div>{getMeetingName(event.meeting_url)}</div>
+                                </div>
+                            }
 
-                        <div className={'card-footer'}>
-                            <div className={'left'}>
-                                <div>{lang['Card_Event_Success_1']} <br/>{lang['Card_Event_Success_2']}</div>
-                                <img src="/images/logo.svg" alt=""/>
+                            <div className={'card-footer'}>
+                                <div className={'left'}>
+                                    <div>{lang['Card_Event_Success_1']} <br/>{lang['Card_Event_Success_2']}</div>
+                                    <img src="/images/logo.svg" alt=""/>
+                                </div>
+                                <QRcode size={[63, 63]}
+                                        text={'https://' + window.location.host + `/event/detail/${params?.eventid}`}/>
                             </div>
-                            <QRcode size={[63, 63]} text={'https://' + window.location.host + `/event/detail/${params?.eventid}`}/>
                         </div>
                     </div>
-                    { !isMobile() &&
+                    {!isMobile() &&
                         <div className={'center'}>
-                            <AppButton special onClick={e => {downloadCard()}}>{lang['Save_Card']}</AppButton>
-                        </div> }
+                            <AppButton special onClick={e => {
+                                downloadCard()
+                            }}>{lang['Save_Card']}</AppButton>
+                        </div>}
                     <div className={'center'}>
-                        <AppButton onClick={e => {copyLink()}}>{lang['IssueFinish_CopyLink']}</AppButton>
+                        <AppButton onClick={e => {
+                            copyLink()
+                        }}>{lang['IssueFinish_CopyLink']}</AppButton>
                     </div>
                 </>
             }
