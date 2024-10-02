37a15312b3497a7c2197d8f318c16c1049ae0544
qieman <253058016@qq.com>
2024-10-03 02:30:14 +0800

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -73,7 +73,6 @@ const walletConnectConnector: any = new CustomWalletConnectConnector({
     options: {
         projectId: '75f461ff2b14465255978cb9e730a6ac',
         qrModalOptions: {
-            enableAuthMode: true,
             enableExplorer: false,
             themeMode:  'light'
         },
