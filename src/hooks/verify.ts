38fdc99b893eebbf2ff04b262be3cfa98dc0f9fc
ZOU <253058016@qq.com>
2025-01-08 16:39:45 +0800

    update create-group

--- src/hooks/verify.ts
+++ src/hooks/verify.ts
@@ -1,5 +1,6 @@
 import { useContext } from 'react'
 import LangContext from '../components/provider/LangProvider/LangContext'
+import fa from "@walletconnect/qrcode-modal/dist/cjs/browser/languages/fa";
 
 
 function useVerify () {
@@ -28,8 +29,8 @@ function useVerify () {
             return lang['Regist_Input_Validate_4']([char[0]])
         }
 
-        if (domain.match(/[`~!@#$%^&*()_+<>?:"{},./;'[\]]/im)) {
-            const char: any = domain.match(/[`~!@#$%^&*()_+<>?:"{},./;'[\]]/im)
+        if (domain.match(/[`~!@#$%^&*()_+<>?:"{},./\\|;'[\]]/im)) {
+            const char: any = domain.match(/[`~!@#$%^&*()_+<>?:"{},./\\|;'[\]]/im)
             return lang['Regist_Input_Validate_4']([char[0]])
         }
 
@@ -48,7 +49,15 @@ function useVerify () {
         return null
     }
 
-    return { verifyDomain }
+    const checkDomainInput =  (domain: string, limitLength=[domainInputMinLength, domainInputMaxLength]) => {
+        if (domain.startsWith('-')) {
+            return false
+        }
+
+        return !domain.match(/[`~!@#$%^&*()_+<>?:"{},./\\|;'[\]]/im);
+    }
+
+    return { verifyDomain, checkDomainInput }
 }
 
 export default useVerify
